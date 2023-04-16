fd98f1e5a75d4c8bff96e30e566efe23e57349bb
puklod <puklod@gmail.com>
2023-04-16 15:48:16 +0200

    menubar alpha

--- js/test.js
+++ js/test.js
@@ -1,1 +1,11 @@
 const navdiv = document.querySelector('#navbar');
+const navicon = document.querySelector('#navicon');
+const menuItems = ["Egy", "Kettő", "Három"];
+
+displayNavicon();
+
+function displayNavicon() {
+    let windowWidth = window.innerWidth;
+
+    if(windowWidth < 550) navicon.style.display = 'none';
+};
