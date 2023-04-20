c355bdafbc8602f6afca560bc5b960276ff7407f
puklod <puklod@gmail.com>
2023-04-20 23:51:51 +0200

    navigacio kinyit becsuk

--- js/test.js
+++ js/test.js
@@ -2,10 +2,12 @@ const navdiv = document.querySelector('#navbar');
 const navicon = document.querySelector('#navicon');
 const menuItems = ["Egy", "Kettő", "Három", "Négy"];
 
-navicon.appendChild(createNavigation());
+navicon.addEventListener('click',() => navigationEvent(navicon));
 
 function createNavigation() {
     const ul = document.createElement('ul');
+        ul.setAttribute('id',"navUl");
+    
     for (const itemName of menuItems) {
         let li = document.createElement('li');
         li.setAttribute('id',itemName);
@@ -13,4 +15,12 @@ function createNavigation() {
     }
 
     return ul;
+}
+
+function navigationEvent(obj) {
+    if(obj.hasChildNodes('ul')){
+        obj.removeChild(obj.childNodes[0]);
+    } else {
+        obj.appendChild(createNavigation());
+    }
 }
